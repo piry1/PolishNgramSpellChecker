d3b36a0d1dbd5dcec19f14bdc4481692b45082c7
piry1 <piry3943@gmail.com>
2018-03-03 17:30:02 +0100

    Add spell checker params

---
+++ PolishNgramSpellChecker/Params/SpellCheckerParams.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PolishNgramSpellChecker.Params
+{
+    public class SpellCheckerParams : ISpellCheckerParams
+    {
+        public int N { get; set; } = 2;
+        public int MinN { get; set; } = 2;
+        public int MaxN { get; set; } = 5;
+        public bool OrderedMatch { get; set; } = false;
+        public Func<double, double, double> ScoreCountFunc { get; set; } = (d, d1) => d;
+    }
+}
